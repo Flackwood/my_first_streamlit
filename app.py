d7900a814283215615871da5db38d2df9dc666bf
Flackwood <94984359+Flackwood@users.noreply.github.com>
2022-02-19 20:32:26 -0500

    aded dragon

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def lottie(url: str):
+    content = load_lottieurl(url)
+    st_lottie(content)
+
 
 st.write('Dear Santa,')
 'This year for Christmas I would like a puppy!'
 ':dog::santa:'
 st.balloons()
+lottie("https://assets5.lottiefiles.com/packages/lf20_2oW79h.json")
